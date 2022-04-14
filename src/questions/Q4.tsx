9a831fba40bbab41a60b621988616d87f2319e14
keeeeesk1114 <keeeeesk1114@gmail.com>
2022-04-14 22:15:26 +0900

    Janken PON!!!!

--- src/questions/Q4.tsx
+++ src/questions/Q4.tsx
@@ -1,6 +1,6 @@
-import { useState } from 'react';
-import Layout from '../components/Layout';
-import '../styles/Q.css';
+import { useState } from "react";
+import Layout from "../components/Layout";
+import "../styles/Q.css";
 /**
  * Q4
  * 問題：じゃんけんをしよう！
@@ -10,18 +10,99 @@ import '../styles/Q.css';
  * by ゆうじろう
  *
  */
+
+enum Janken {
+  ROCK,
+  SCISSORS,
+  PAPER,
+}
+
+const youLoseLabel: String = "YOU LOSE!!!!!!!";
+const youWinLabel: String = "YOU WIN!";
+const evenLabel: String = "あいこ";
+
+const createOponentsHand = (): Janken => {
+  const oponentsHand = Math.floor(Math.random() * 3);
+  if (oponentsHand === 0) {
+    return Janken.ROCK;
+  }
+  if (oponentsHand === 1) {
+    return Janken.SCISSORS;
+  }
+  return Janken.PAPER;
+};
+
+const battle = (yourHand: Janken, oponentsHand: Janken): String => {
+  if (yourHand === oponentsHand) {
+    return evenLabel;
+  }
+
+  switch (oponentsHand) {
+    case Janken.ROCK:
+      return yourHand === Janken.PAPER ? youWinLabel : youLoseLabel;
+    case Janken.SCISSORS:
+      return yourHand === Janken.ROCK ? youWinLabel : youLoseLabel;
+    case Janken.PAPER:
+      return yourHand === Janken.SCISSORS ? youWinLabel : youLoseLabel;
+  }
+};
+
+const getJankenLabel = (janken: Janken): String => {
+  switch (janken) {
+    case Janken.ROCK:
+      return "ぐー";
+    case Janken.SCISSORS:
+      return "ちょき";
+    case Janken.PAPER:
+      return "ぱー";
+  }
+};
+
 const Q4 = () => {
+  const [yourHand, setYourHand] = useState(0);
+  const [opponentsHand, setOponentsHand] = useState(0);
+  const [isBeforeBattle, setIsBeforeBattle] = useState(false);
+
   return (
     <Layout>
       <main className="questionWrapper">
         <div className="questionZone">
-          <h1>src/questions/Q4.tsx じゃんけんプログラムを実装してください！</h1>
-          <button>ぐー</button>
-          <button>ちょき</button>
-          <button>ぱー</button>
-          <h1>あなたの手 :</h1>
-          <h1>CPUの手 :</h1>
-          <h1>勝敗 : </h1>
+          <button
+            onClick={() => {
+              setYourHand(Janken.ROCK);
+              setOponentsHand(createOponentsHand());
+              setIsBeforeBattle(true);
+            }}
+          >
+            {getJankenLabel(Janken.ROCK)}
+          </button>
+          <button
+            onClick={() => {
+              setYourHand(Janken.SCISSORS);
+              setOponentsHand(createOponentsHand());
+              setIsBeforeBattle(true);
+            }}
+          >
+            {getJankenLabel(Janken.SCISSORS)}
+          </button>
+          <button
+            onClick={() => {
+              setYourHand(Janken.PAPER);
+              setOponentsHand(createOponentsHand());
+              setIsBeforeBattle(true);
+            }}
+          >
+            {getJankenLabel(Janken.PAPER)}
+          </button>
+          <h1>
+            {isBeforeBattle ? "あなたの手 : " + getJankenLabel(yourHand) : ""}
+          </h1>
+          <h1>
+            {isBeforeBattle ? "CPUの手 : " + getJankenLabel(opponentsHand) : ""}
+          </h1>
+          <h1>
+            {isBeforeBattle ? "勝敗 : " + battle(yourHand, opponentsHand) : ""}
+          </h1>
         </div>
       </main>
     </Layout>
