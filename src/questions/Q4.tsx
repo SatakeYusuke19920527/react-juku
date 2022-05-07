eb32831e1647eb5041c4587225d8984d83b41ef7
kenta715 <benrimail1@gmail.com>
2022-05-07 21:33:21 +0900

    Q4回答途中

--- src/questions/Q4.tsx
+++ src/questions/Q4.tsx
@@ -11,20 +11,63 @@ import '../styles/Q.css';
  *
  */
 const Q4 = () => {
-  const handleGu = (te: string) => {
-    console.log(te);
+  const [myhand, setMyHand] = useState<string>();
+  const [cpuhand, setCpuHand] = useState<string>();
+  const [result, setResult] = useState<string>();
+  const handleTe = (te: string) => {
+    setMyHand(te);
+    setCpuHand(createCpuHand());
+    setResult(createResult());
   };
+
+  const createCpuHand = () => {
+    const randomNum = Math.floor( Math.random() * 3);
+    if (randomNum === 0) {
+      return "ぐー";
+    }
+    if(randomNum === 1) {
+      return "ぱー";
+    }
+    return "ちょき";
+  };
+
+  const createResult = () => {
+    console.log(myhand);
+    console.log(cpuhand);
+    if (myhand === cpuhand) {
+      return "あいこ";
+    }
+    if(myhand === "ぐー"){
+      if(cpuhand === "ぱー"){
+        return "あなたの負け";
+      }
+      return "あなたの勝ち";
+    }
+    if(myhand === "ぱー"){
+      if(cpuhand === "ちょき"){
+        return "あなたの負け";
+      }
+      return "あなたの勝ち";
+    }
+    if(myhand === "ちょき"){
+      if(cpuhand === "ぐー"){
+        return "あなたの負け";
+      }
+      return "あなたの勝ち";
+    }
+  }
+
   return (
     <Layout>
       <main className="questionWrapper">
         <div className="questionZone">
           <h1>src/questions/Q4.tsx じゃんけんプログラムを実装してください！</h1>
-          <button onClick={() => handleGu('gu')}>ぐー</button>
-          <button>ちょき</button>
-          <button>ぱー</button>
-          <h1>あなたの手 :</h1>
-          <h1>CPUの手 :</h1>
-          <h1>勝敗 : </h1>
+          <button onClick={() => handleTe('ぐー')}>ぐー</button>
+          <button onClick={() => handleTe('ちょき')}>ちょき</button>
+          <button onClick={() => handleTe('ぱー')}>ぱー</button>
+          <h1>あなたの手 : {myhand}</h1>
+          <h1>CPUの手 :{cpuhand}</h1>
+          <h1>勝敗 : {result}</h1>
         </div>
       </main>
     </Layout>
